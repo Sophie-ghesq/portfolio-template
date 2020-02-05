db358ce6c63080c08bad8213a92c449c7c915bc3
Sophie-ghesq <sophieghesq@gmail.com>
2020-02-05 09:58:15 +0100

    Update 01-projet.php

--- vue/projet-single/01-projet.php
+++ vue/projet-single/01-projet.php
@@ -11,10 +11,8 @@
   	<!-- header -->
 	<header>
 		<div class="parralax"></div>
-			<div class="opacityheader"> 
-				<h1>Nano Beach Club</h1> 
-				<h3 class="normal">Webdesign</h3>   
-			</div>	
+			<h1>Titre du projet</h1> 
+			<h3 class="normal">description</h3>   
 	</header>
   <!-- main -->
  	<main>
@@ -22,32 +20,32 @@
 	  		<div class="row">
 			<!-- images projets -->
 				<div class="col-lg-6 col-12 img-projet-single">
-					<img class="img-fluid" src="../../src/img/portfolio/01-nano-item01.png" alt="nano">
+					<img class="img-fluid" src="" alt="Projet-background">
 				</div>
 				<div class="offset-lg-1 col-lg-5 block-fixed">
 				<!-- textes projets-->
-				<h2>Nanö Beach Club</h2>
+				<h2>Projet</h2>
 				<p>
-					<span class="bold">Subauma, Bahia, Brésil</span>
+					<span class="bold">lieu du projet</span>
 				</p>
-				<p>Nanö Beach Club est un hôtel tout confort située les pieds dans l'eau. C'est aussi un mélange entre l'ambiance des nuits "paulistana" avec l'énergie de Tommorrowland.</p>
-				<p>Création d'une double identité "noite beach club" et hotel.</p>  
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo arcu eget purus vehicula facilisis non in eros. Curabitur commodo, dui et commodo hendrerit, est ipsum bibendum ante, sit amet sollicitudin est turpis in massa. Praesent ac elit nec ante rhoncus consequat sed ut eros.</p>
+				<p> In eleifend erat consequat libero pretium, sed vehicula turpis consequat.</p>  
 				<!-- annotations -->
 				<ul>
 					<li>
 						<h3>Rôle:</h3>
-						<p>Webdesign, UI & UX design</p>
+						<p>Lorem ipsum dolor sit amet</p>
 					</li>
 					<li>
 						<h3>Credits:</h3>
-						<p>Réalisé sous la Direction Artisitique de 
-						<a class="underline" href="https://www.regularswitch.com/#!/"> 
-					Regular Switch 
+						<p>Lorem ipsum dolor sit amet
+						<a class="underline" href=""> 
+					Lorem
 						</a></p>
 					</li>
 				</ul>
 				<p>
-					<a id="websit-lien" href="http://www.nanobeach.com.br/">Visit Website</a>
+					<a id="websit-lien" href="">Visit Website</a>
 				</p>
 				<!-- fermeture projets-->
 				</div>
